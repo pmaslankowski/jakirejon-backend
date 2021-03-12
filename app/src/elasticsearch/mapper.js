dcab90f002062c0e56a4acb1c8df982fe3bee7b1
Piotr Maślankowski <pmaslankowski@advatech.pl>
2021-03-12 07:49:52 +0000

    fix: handling empty apartments in mapper

--- app/src/elasticsearch/mapper.js
+++ app/src/elasticsearch/mapper.js
@@ -5,6 +5,14 @@ export const mapToAddress = hit => {
   const addressDoc = hit._source;
   const hospitalDoc = addressDoc.hospital;
   const hospital = new Hospital(hospitalDoc.name, hospitalDoc.address, hospitalDoc.phone);
-  const apartments = addressDoc.apartments.map(x => x.toString());
+  const apartments = mapApartments(addressDoc.apartments);
   return new Address(addressDoc.street, addressDoc.district, hospital, apartments);
+}
+
+const mapApartments = apartments => {
+  if (apartments == null) {
+    return null;
+  } else {
+    return apartments.map(x => x.toString());
+  }
 }
--- app/src/elasticsearch/mapper.test.js
+++ app/src/elasticsearch/mapper.test.js
@@ -23,9 +23,35 @@ describe('mapToAddress', () => {
       'Gajowa', 
       'Krzyki', 
       new Hospital('Szpital', 'Szpitalna 1', '123 456 789'), 
-      [1,2,3]
+      ['1', '2', '3']
     );
   
     expect(address).toEqual(expected);
   });
+
+  it('should handle empty apartments', () => {
+    const document = {
+      _source: {
+        street: 'Gajowa',
+        district: 'Krzyki',
+        hospital: {
+          name: 'Szpital',
+          address: 'Szpitalna 1',
+          phone: '123 456 789'
+        },
+        apartments: null
+      }
+    };
+
+    const address = mapToAddress(document);
+
+    const expected = new Address(
+      'Gajowa', 
+      'Krzyki', 
+      new Hospital('Szpital', 'Szpitalna 1', '123 456 789'), 
+      null
+    );
+
+    expect(address).toEqual(expected);
+  })
 })
